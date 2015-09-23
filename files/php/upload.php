929a7115fe5014d955361e913cbdb1f3fc74f0c2
oscar <oscar.luis.garcia94@gmail.com>
2015-09-23 13:15:31 -0500

    solo falta agregar campos de formulario  editar , probar xmla

--- files/php/upload.php
+++ files/php/upload.php
@@ -38,6 +38,12 @@ if ($uploadOk == 0) {
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+
+
+
+
+
+
         header("Location: ../../admin/new.php");
     } else {
         echo "Sorry, there was an error uploading your file.";
